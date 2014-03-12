4a5b56725243e42d9798a87249273bd97913b98c
Jarred Sumner <jarred@lockitron.com>
2014-03-12 10:19:03 -0400

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Mails::Application.config.secret_key_base = '8807d6f934e3afd9a9a708f7fd35e80bdf13a9de1be07c92d9aeafa2bdf777a934820d4ab30815ba74ecd2f13a987e9069e84e94a552ec1507584d33edff8f9a'
